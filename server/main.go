8f537c35f3545fd408eda829f485464d1456e9f1
g-vit <gutsalyuk.vitaliy@gmail.com>
2020-08-21 17:10:18 +0300

    add server

--- server/main.go
+++ server/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	ctx "context"
+	"fmt"
 	"net"
 
+	pb "github.com/g-vit/simple-grpc/hello"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/grpclog"
 )
+
+type server struct{}
 
 func main() {
 	lis, err := net.Listen("tcp", ":9292")
@@ -14,5 +19,14 @@ func main() {
 	}
 	opts := []grpc.ServerOption{}
 	grpcServer := grpc.NewServer(opts...)
+	pb.RegisterHelloServer(grpcServer, &server{})
 	grpcServer.Serve(lis)
+}
+
+func (s *server) Do(c ctx.Context, request *pb.Request) (response *pb.Response, err error) {
+	msg := request.Message
+	response = &pb.Response{
+		Message: fmt.Sprintf("Hello %s", msg),
+	}
+	return response, nil
 }
